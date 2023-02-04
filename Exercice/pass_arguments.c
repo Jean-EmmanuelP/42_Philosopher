a22cee117465b86cadf0a6f79306395986ac61e6
Jean-Emmanuel Perramant <perramant.work@gmail.com>
2023-02-04 14:42:45 +0100

    new commit

--- Exercice/pass_arguments.c
+++ Exercice/pass_arguments.c
@@ -7,22 +7,36 @@ int primes[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
 
 void *routine(void *args)
 {
-    int index =*(int *)arg;
-    printf("%d", primes[index]);
+    int index =*(int *)args;
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        sum += primes[index + i];
+    }
+    *(int *)args = sum;
+    return args;
 }
 
 int main(int ac, char **av)
 {
-    pthread_t th[10];
-    for (int i = 0; i < 10; i++)
+    pthread_t th[2];
+    int i;
+    for (i = 0; i < 2; i++)
     {
-        if (pthread_create(&th[i], NULL, &routine, &i))
+        int *a = malloc(sizeof(int));
+        *a = i * 5;
+        if (pthread_create(&th[i], NULL, &routine, a))
             perror("Failed to create thread");
     }
-    for (int i = 0l i < 10; i++)
+    int globalSum = 0;
+    for (i = 0; i < 2; i++)
     {
-        if (pthread_join(th[i], NULL))
+        void *r;
+        if (pthread_join(th[i], &r))
             perror("Failed to join thread");
+        globalSum += *(int *)r;
+        free(r);
     }
+    printf("Global sum: %d\n", globalSum);
     return 0;
 }
